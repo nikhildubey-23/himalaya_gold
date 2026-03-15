d711b30a73672c3bd65e2350f0c5084422b61606
nikhildubey-23 <ravannick0089@gmail.com>
2026-03-15 20:11:33 +0530

    push time

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/robots.txt')
+def robots_txt():
+    return send_from_directory(app.root_path, 'robots.txt')
+
+@app.route('/sitemap.xml')
+def sitemap_xml():
+    return send_from_directory(app.root_path, 'sitemap.xml')
 
 if __name__ == '__main__':
     app.run(debug=True)
